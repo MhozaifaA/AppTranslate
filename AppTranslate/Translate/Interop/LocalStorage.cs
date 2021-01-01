a21abdefb4e09b0b480e6d5688edfa6fc6a7d9d9
MHozaifaA <botchey.2012@gmail.com>
2021-01-01 15:01:44 +0200

    refactory localstorage class

--- AppTranslate/Translate/Interop/LocalStorage.cs
+++ AppTranslate/Translate/Interop/LocalStorage.cs
@@ -16,7 +16,12 @@ namespace AppTranslate.Translate.Interop
     {
         private readonly IJSRuntime jsRuntime;
         private readonly IJSInProcessRuntime jSInProcessRuntime;
-        private bool IsInjected=true;
+
+        private const string js_localStorage_setItem = "localStorage.setItem";
+        private const string js_localStorage_getItem = "localStorage.getItem";
+        private const string default_localStorage_Key = "Translate";
+
+        private bool IsInjected=true;// default client side true
 
         public LocalStorage(IJSRuntime jsRuntime)
         {
@@ -26,96 +31,122 @@ namespace AppTranslate.Translate.Interop
 
         public LocalStorage(IJSRuntime jsRuntime, IOptions<LocalStorageOptions> options) : this(jsRuntime)
         => IsInjected = !options.Value.IsServerSide;
-        
-        
 
 
-        public  void SetItem(string value)
+        #region -   SetItem   -
+
+        private string SerializeValue<T>(T value)
+        {
+            if (value is string str)
+                return str;
+            else
+                return JsonSerializer.Serialize(value);
+        }
+
+        #region Sync
+        public void SetItem(string value)
+        {
+            if (ThrowUnRenderInject()) return;
+            SetItem(default_localStorage_Key, value);
+        }
+        public void SetItem(string key, string value)
+        {
+            if (ThrowUnRenderInject()) return;
+            SetItem<string>(key, value);
+        }
+        public void SetItem<T>(string key, T value)
         {
             if (ThrowUnRenderInject()) return;
 
-            if (jSInProcessRuntime is not null)
-                jSInProcessRuntime.InvokeVoid("localStorage.setItem", "Translate", value);
-            else
-                jsRuntime.InvokeVoidAsync("localStorage.setItem", "Translate", value);
-        }
-
-        public string GetItem(string @default=null)
-        {
-            if (ThrowUnRenderInject()) return default;
+            string _value = SerializeValue(value);
 
             if (jSInProcessRuntime is not null)
-                return jSInProcessRuntime.Invoke<string>("localStorage.getItem", "Translate");
+                jSInProcessRuntime.InvokeVoid(js_localStorage_setItem, key, _value);
             else
-                return @default;
-                //return (string) Task.Factory.StartNew(async ()=>await jsRuntime.InvokeAsync<object>("localStorage.getItem", "Translate")).Result.Result;
+                jsRuntime.InvokeVoidAsync(js_localStorage_setItem, key, _value);
         }
+        #endregion
 
-
-
-
+        #region Async
         public async ValueTask SetItemAsync(string value)
         {
             if (ThrowUnRenderInject()) return;
-
-            await jsRuntime.InvokeVoidAsync("localStorage.setItem", "Translate", value).ConfigureAwait(false);
+            await SetItemAsync(default_localStorage_Key, value);
         }
-
-        public async ValueTask<string> GetItemAsync()
+        public async ValueTask SetItemAsync(string key, string value)
         {
-            if (ThrowUnRenderInject()) return default;
-
-            return (await jsRuntime.InvokeAsync<string>("localStorage.getItem", "Translate").ConfigureAwait(false));
+            if (ThrowUnRenderInject()) return;
+            await SetItemAsync<string>(key, value);
         }
-     
-
-
-
-
-
         public async ValueTask SetItemAsync<T>(string key, T value)
         {
             if (ThrowUnRenderInject()) return;
-
-            string _value = JsonSerializer.Serialize(value); 
-            await jsRuntime.InvokeVoidAsync("localStorage.setItem", key, _value).ConfigureAwait(false);
+            string _value = SerializeValue(value);
+            await jsRuntime.InvokeVoidAsync(js_localStorage_setItem, key, _value).ConfigureAwait(false);
         }
+        #endregion
 
+        #endregion
 
-        public async ValueTask<T> GetItemAsync<T>(string key)
+        #region -   GetItem   -
+
+        #region Sync
+        public string GetItem()
+        {
+            if (ThrowUnRenderInject()) return default;
+            return GetItem(default_localStorage_Key);
+        }
+        public string GetItem(string key)
+        {
+            if (ThrowUnRenderInject()) return default;
+            return GetItem<string>(key);
+        }
+        public T GetItem<T>(string key)
         {
             if (ThrowUnRenderInject()) return default;
 
-            var value= await jsRuntime.InvokeAsync<string>("localStorage.getItem", key).ConfigureAwait(false);
-            return JsonSerializer.Deserialize<T>(value);
+            if (jSInProcessRuntime is not null)
+                return jSInProcessRuntime.Invoke<T>(js_localStorage_getItem, key);
+            else
+                return default(T);
+            //return Task.Factory.StartNew(async ()=>await jsRuntime.InvokeAsync<T>(js_localStorage_getItem, default_localStorage_Key)).Result.Result;
         }
+        #endregion
 
-
-        public ValueTask ConsoleLog(string message)
+        #region Async
+        public async ValueTask<string> GetItemAsync()
         {
-            if (ThrowUnRenderInject())  return default;
-
-            return jsRuntime.InvokeVoidAsync("console.log", message);
+            if (ThrowUnRenderInject()) return default;
+            return await GetItemAsync(default_localStorage_Key);
         }
+        public async ValueTask<string> GetItemAsync(string key)
+        {
+            if (ThrowUnRenderInject()) return default;
+            return await GetItemAsync<string>(key);
+        }
+        public async ValueTask<T> GetItemAsync<T>(string key)
+        {
+            if (ThrowUnRenderInject()) return default;
+            return await jsRuntime.InvokeAsync<T>(js_localStorage_getItem, key).ConfigureAwait(false);
+        }
+        #endregion
 
+        #endregion
 
+        #region -   Util   -
+
+        public ValueTask ConsoleLog(params string[] messages)
+        {
+            if (ThrowUnRenderInject()) return default;
+            return jsRuntime.InvokeVoidAsync("console.log", messages);
+        }
         private bool ThrowUnRenderInject()
-        {
-            if (!IsInjected)
-                return true;
-             return false;
-               // throw new ArgumentException(nameof(ThrowUnRenderInject) + " AfterRender Translate.Inject()");
-        }
-
+          => !IsInjected; // throw new ArgumentException(nameof(ThrowUnRenderInject) + " AfterRender Translate.Inject()");
         public void Inject()
-        {
-            IsInjected = true;
-        }
+           => IsInjected = true;
 
-        public LocalStorage File() // and code
-        {
-            return this;
-        }
+        #endregion
+
 
     }
 
